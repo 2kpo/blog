1c1e7539111e36202c45aa16dd039b7682ca7917
2kpo <2kpo@2kpodeMacBook-Pro.local>
2017-09-05 13:33:41 +0800

    install passport cors package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,14 +81,13 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <example></example>
+                <div class="container">
+                    <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
+
             </div>
         </div>
     </body>
